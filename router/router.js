991c58ebeadf32ba8c61f7d249d7a207a36d2786
Samuel Goupil <>
2021-04-01 22:01:16 +0200

    article

--- router/router.js
+++ router/router.js
@@ -1,15 +1,13 @@
 let HomeController = require('./../controllers/HomeController');
 let VipController = require('./../controllers/VipController');
 let AlbumController = require('./../controllers/AlbumController');
-let TestController = require('./../controllers/TestController');
 let ArticleController = require('./../controllers/ArticleController');
 
 
 // Routes
 module.exports = function(app){
 
-  // tests à supprimer
-    app.get('/test', TestController.Test);
+
 
 // Main Routes
     app.get('/', HomeController.Index);
